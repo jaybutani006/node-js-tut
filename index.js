a1b5eeb0ba13a35366160bf4ac8bba3fd9369b42
Jay Butani <jaybutani310@gmail.com>
2022-08-27 12:31:42 +0530

    co

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ console.log(app.x)
 console.log(app)
 console.log(app.z(5, 4))
 
+// for install node moduls just write npm istall
+// it will install all the modules which is in the package.json file having.
 let arr = [3, 4, 8, 2, 9, 1, 9, 2, 3, 4, 1];
 
 let result = arr.filter((item) => {
